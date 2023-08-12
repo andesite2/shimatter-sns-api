a5f9e4d316d521fe994a84a4c75bbdba4989a079
Nnull1234 <zinnsuke12@gmail.com>
2023-08-12 16:13:16 +0900

    dwa

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const POST = 8000;
+const POST = process.env.POST || 8000;
 app.use(cors());
 app.use(express.json());
 
